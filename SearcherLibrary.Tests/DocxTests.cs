82e771cc09c6b3b6fcef06621c8d4610ebf11bcf
Dennis Joseph <wizden@gmail.com>
2022-01-18 17:37:45 +0000

    Updated to add tests for .docx format.

--- SearcherLibrary.Tests/DocxTests.cs
+++ SearcherLibrary.Tests/DocxTests.cs
@@ -5,12 +5,12 @@ using Xunit;
 
 namespace SearcherLibrary.Tests
 {
-    public class TextFileTests
+    public class DocxTests
     {
         #region Private Fields
 
         private static string rootDirectory = "FilesToTest";
-        string filePath = Path.Combine(Directory.GetParent(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath).FullName, rootDirectory, "Txt.txt");
+        string filePath = Path.Combine(Directory.GetParent(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath).FullName, rootDirectory, "Docx.docx");
 
         #endregion Private Fields
 
@@ -21,7 +21,7 @@ namespace SearcherLibrary.Tests
         {
             var test = File.ReadAllText(filePath);
             var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "the" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.IgnoreCase });
-            Assert.Equal(2, matchedLines.Count);
+            Assert.True(matchedLines.Count == 2);
         }
 
         [Fact]
@@ -29,7 +29,7 @@ namespace SearcherLibrary.Tests
         {
             var test = File.ReadAllText(filePath);
             var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "The" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.None });
-            Assert.Single(matchedLines);
+            Assert.True(matchedLines.Count == 1);
         }
 
         [Fact]
@@ -37,23 +37,16 @@ namespace SearcherLibrary.Tests
         {
             var test = File.ReadAllText(filePath);
             var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "th.*qu" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.Singleline | System.Text.RegularExpressions.RegexOptions.IgnoreCase });
-            Assert.Single(matchedLines);
-        }
-
-        [Fact]
-        public void SearchText_Regex_CaseSensitive_Multiline_MatchesOne()
-        {
-            var test = File.ReadAllText(filePath);
-            var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "The(.|\n)*?fox" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.Multiline | System.Text.RegularExpressions.RegexOptions.IgnoreCase });
-            Assert.Single(matchedLines);
+            Assert.True(matchedLines.Count == 1);
         }
 
         [Fact]
         public void SearchText_Regex_CaseInsensitive_Multiline_MatchesThree()
         {
-            var test = File.ReadAllText(filePath);
-            var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "e(.|\n)*?o" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.Multiline | System.Text.RegularExpressions.RegexOptions.IgnoreCase });
-            Assert.Equal(3, matchedLines.Count);
+            // TODO: Determine if there is a way to give multi-line regex with sensible return value to client.
+            //var test = File.ReadAllText(filePath);
+            //var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "e(.|\n)*?o" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.Multiline | System.Text.RegularExpressions.RegexOptions.IgnoreCase });
+            //Assert.True(matchedLines.Count == 3);
         }
 
         [Fact]
@@ -61,7 +54,7 @@ namespace SearcherLibrary.Tests
         {
             var test = File.ReadAllText(filePath);
             var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "Th.*qu" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.Singleline });
-            Assert.Single(matchedLines);
+            Assert.True(matchedLines.Count == 1);
         }
 
         [Fact]
@@ -69,7 +62,7 @@ namespace SearcherLibrary.Tests
         {
             var test = File.ReadAllText(filePath);
             var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "the", "quick"}, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.IgnoreCase });
-            Assert.Equal(3, matchedLines.Count);
+            Assert.True(matchedLines.Count == 3);
         }
 
         [Fact]
@@ -77,7 +70,7 @@ namespace SearcherLibrary.Tests
         {
             var test = File.ReadAllText(filePath);
             var matchedLines = FileSearchHandlerFactory.Search(filePath, new string[] { "the", "quick" }, new Matcher { RegexOptions = System.Text.RegularExpressions.RegexOptions.None });
-            Assert.Equal(2, matchedLines.Count);
+            Assert.True(matchedLines.Count == 2);
         }
 
         #endregion Public Methods
